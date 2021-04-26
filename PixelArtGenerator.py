6c8c05b1755807eee4ddfab2d731cbed1f896509
José Bernardo Girón Arana <bernardo.giron99@gmail.com>
2021-04-26 15:23:24 -0500

    Add function to create pixel art and function to resize square images

---
+++ PixelArtGenerator.py
@@ -0,0 +1,41 @@
+from PIL import Image, ImageDraw, ImageFont
+import random
+
+
+FUNKYFUTURE_x8 = (["#2b0f54","#ab1f65","#ff4f69","#fff7f8","#ff8142","#ffda45","#3368dc","#49e7ec"],8)
+
+
+def generatePixelImage(pallete,imageWidth, imageHeight,fileName):
+
+    colors, size = pallete
+    images = [Image.new( "RGB",(1,1), color = colors[random.randint(0,len(colors)-1)] ) for amount in range(size * size)]
+    pixelImage = Image.new("RGB",(size * imageWidth,size * imageHeight))
+    
+    if imageHeight == imageWidth:
+        fillSquareImage(pixelImage,images,imageWidth, imageHeight).save(fileName,quality = 100)
+
+def fillSquareImage(pixelImage,images,resizeRateWidth, resizeRateHeight):
+    row = 0
+    while row < pixelImage.height:
+        column = 0
+
+        while column < pixelImage.width:
+
+            current_color = images.pop(0)
+            countR = row  + resizeRateHeight - 1
+
+            while countR >= row:
+                countC = column + resizeRateWidth - 1
+
+                while countC >= column:
+                    pixelImage.paste(current_color,(countR,countC)) 
+                    countC -= 1
+
+                countR -= 1
+            column += resizeRateWidth
+        row += resizeRateHeight
+    return pixelImage
+
+
+generatePixelImage(FUNKYFUTURE_x8,50,50,"funkyFuture.png")
+
